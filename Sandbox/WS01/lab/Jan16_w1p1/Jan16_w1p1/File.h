476761a76a03106ef2305c8bc73b01156054a59f
Soyon Lee <slee550@myseneca.ca>
2023-01-16 12:20:38 -0500

    create modules movie, file, 50best

---
+++ Sandbox/WS01/lab/Jan16_w1p1/Jan16_w1p1/File.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef STD_FILE_H
+#define STD_FILE_H
+#ifndef SDDS_FILE_H
+#define SDDS_FILE_H
+
+bool openFile(const char filename[]);
+void closeFile();
+bool readTitle(char title[]);
+bool readYear(int* year);
+bool readMovieRating(char rating[]);
+bool readDuration(int* duration);
+bool readGenres(char genre[][11]);
+bool readConsumerRating(float* rating);
+
+#endif
+#endif
---
+++ Sandbox/WS01/lab/Jan16_w1p1/Jan16_w1p1/Movie.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef STD_MOVIE_H
+#define STD_MOVIE_H
+#ifndef SDDS_MOVIE_H
+#define SDDS_MOVIE_H
+
+struct Movie {
+	char m_title[128];
+	int m_year;
+	char m_rating[6];
+	int m_duration;
+	char m_genres[10][11];
+	float m_consumerRating;
+};
+
+bool loadMovies();
+bool hasGenre(const Movie* mvp, const char genre[]);
+void displayMovie(const Movie* mvp);
+void displayMoviesWithGenre(const char genre[]);
+
+#endif
+#endif
