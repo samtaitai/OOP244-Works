6a12f962be51031433e988c815f08f750b5346f7
Soyon Lee <slee550@myseneca.ca>
2023-02-08 09:34:47 -0500

    fix collision

---
+++ Sandbox/WS04/lab/Soccer.h
@@ -0,0 +1,42 @@
+/*
+Name: So Yon Lee
+Email: slee550@myseneca.ca
+Student ID: 179142211
+Date: 8 Feb 2023
+I have done all the coding by myself
+and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+#ifndef SDDS_SOCCERTEAM_H_
+#define SDDS_SOCCERTEAM_H_
+#include <iostream>
+
+namespace sdds {
+
+	const int MAX_FOUL = 4; //maximum number of fouls
+
+	struct SoccerTeam
+	{
+		char m_teamName[41];	
+		int m_noFouls;			
+		double m_fines;			
+		int m_goals;			
+	
+		SoccerTeam();
+		SoccerTeam(const char* tname, double fines, int foul);
+
+		void setTeam(const SoccerTeam& team);
+		void setName(const char* tname);
+		void setFine(double fines, int foul);
+
+		void setEmpty();
+		bool isEmpty() const;
+		void calFines();			
+		int fouls() const;
+		std::ostream& display()const; //??
+	};
+
+}
+
+#endif
+
+
