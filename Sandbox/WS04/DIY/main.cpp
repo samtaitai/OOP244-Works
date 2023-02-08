b1e059ce24d7ab21e3d51c026e79b14ceef8f8f2
Soyon Lee <slee550@myseneca.ca>
2023-02-08 15:21:45 -0500

    drawing box

--- Sandbox/WS04/DIY/main.cpp
+++ Sandbox/WS04/DIY/main.cpp
@@ -18,21 +18,26 @@ using namespace std;
 #include "NameTag.h"
 using namespace sdds;
 int main() {
+
+   //without input(constructor)
    NameTag nt[6] = {  
-      "A very very very long name to be cut short!!!", // one argument constructor
-      {"Fred Soley", 12345} ,       // two argument constructor with good data
-      {nullptr, 23456},    // two argument constructor with bad name
-      {"Bad number", 1234},  // two argument constructor with bad number
-      {"Bad number", 123456}  // two argument constructor with bad number
-                             // default constructor (sixth element)
+      "A very very very long name to be cut short!!!",  // one argument constructor
+      {"Fred Soley", 12345}                            // two argument constructor with good data
+      //{nullptr, 23456},                                 // two argument constructor with bad name
+      //{"Bad number", 1234},                             // two argument constructor with bad number
+      //{"Bad number", 123456},                           // two argument constructor with bad number
+                                                        // default constructor (sixth element)
    };
    int i;
    for (i = 0; i < 6; i++) {
       nt[i].print();
       cout << endl;
    }
-   nt[1].read().print();
-   nt[3].read().print();
-   nt[5].read().print();
+
+   //with input 
+   //nt[1].read().print(); //{"Fred Soley", 12345}
+   //nt[3].read().print(); //{"Bad number", 1234}
+   //nt[5].read().print(); //{}
+
    return 0;
 }
