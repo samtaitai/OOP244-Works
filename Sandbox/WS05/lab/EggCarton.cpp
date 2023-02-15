f84d3d46af7e9555b102b55a46765de294984a32
Soyon Lee <slee550@myseneca.ca>
2023-02-15 16:41:13 -0500

    ready to submit

--- Sandbox/WS05/lab/EggCarton.cpp
+++ Sandbox/WS05/lab/EggCarton.cpp
@@ -2,7 +2,7 @@
 Name: So Yon Lee
 Email: slee550@myseneca.ca
 Student ID: 179142211
-Date:
+Date: 15 Feb 2023
 I have done all the coding by myself
 and only copied the code that my professor provided to complete my workshops and assignments.
 */
