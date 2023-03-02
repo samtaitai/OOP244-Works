56677eb75aac48992bff0c85b45feb3f518e11e0
Soyon Lee <slee550@myseneca.ca>
2023-03-02 17:40:06 -0500

    Number.h

--- Sandbox/WS06/lab/Numbers.h
+++ Sandbox/WS06/lab/Numbers.h
@@ -3,10 +3,36 @@
 #include <iostream>
 namespace sdds {
    class Numbers {
-      unsigned int countLines(const char* filename);
-      void sort(double* collectionPtr, unsigned int size);
+
+       double* m_collection;
+       char m_filename[255];
+       int m_collectionSize;
+       bool m_originalFlag;
+       bool m_addedFlag;
+
+       unsigned int countLines(const char* filename);
+       void sort(double* collectionPtr, unsigned int size);
+       void setEmpty();
+       bool load();
+       Numbers& save();
+       double max() const;
+       double min() const;
+       double average() const;
+
    public:
+       Numbers();
+       Numbers(char* filename);
+       ~Numbers();
+       Numbers(Numbers& N);
+       Numbers& operator=(Numbers& N);
+       operator bool() const;
+       Numbers& sort();
+       Numbers& operator+=(Numbers& RoN);
+       std::ostream& display(std::ostream& ostr = std::cout) const;
    };
+
+   std::ostream& operator<<(std::ostream& ostr, Numbers& RoN);
+   std::istream& operator>>(std::istream& istr, Numbers& RoN);
 
 }
 #endif // !SDDS_NUMBERS_H_
