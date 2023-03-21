7da13d1cfcf002d2c2fbe6884d3763211b92fd3c
Soyon Lee <slee550@myseneca.ca>
2023-03-21 13:32:40 -0400

    ws08: citation

--- Sandbox/WS08/lab/Media.h
+++ Sandbox/WS08/lab/Media.h
@@ -1,3 +1,11 @@
+/*
+Name: So Yon Lee
+Email: slee550@myseneca.ca
+Student ID: 179142211
+Date: 21 Mar 2023
+I have done all the coding by myself
+and only copied the code that my professor provided to complete my workshops and assignments.
+*/
 #ifndef SDDS_MEDIA_H
 #define SDDS_MEDIA_H
 #include <iostream>
