600ac523b0114a6f2c70fdfc25d1e0bd67be0a31
Soyon Lee <slee550@myseneca.ca>
2023-03-21 10:33:08 -0400

    MS3 updated

--- Sandbox/MS3/main.cpp
+++ Sandbox/MS3/main.cpp
@@ -29,7 +29,7 @@ void displayFile(const char* name) {
 }
 Tool getTool() {
    Tool T;
-   cout << "Please enter the Tool infomation: " << endl;
+   cout << "Please enter the Tool information: " << endl;
    cin >> T;
    return T;
 }
@@ -100,7 +100,7 @@ int main() {
          if(M)
             cout << M << endl;
          else
-            cout << "Rocord number " << cnt++ << " " << M << endl;
+            cout << "Record number " << cnt++ << " " << M << endl;
       }
    }
    return 0;
